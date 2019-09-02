ad0c0f08da122734ab7113f202566d7352840758
Bryson Creighton <brysoncreighton@gmail.com>
2019-09-02 16:24:23 -0600

    Added user stories to main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+/* User Stories
+        A user will be presented a start button and description of the game upon page load.
+
+        When start is clicked the game will start.
+
+        A question will be populated on the page.
+
+        The total amount of questions will be shown in the Question Tracker.
+
+        One of four answers can be selected for any given question.
+
+        Upon submission the next question will be presented to the user.
+
+        The question tracker will update after each question; showing the current question # out of the total number of questions, along with updating the just answered question with green or red to provide correct or incorrect feedback for the answer that was just submitted.
+
+        At the end of the game the user will be presented their final score.
+            7 or less will render failure text
+            8 or more will render successful text
+        
+        User will be presented with a button to play again.
+
+        No navigation of quesitons!!!
+*/
